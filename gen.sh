d17c6367d2934b60b31a693412c9e9d299b6d29a
Ulysse Corbeil <ulysse98@hotmail.com>
2025-08-28 15:26:39 -0400

    remove bad pbs

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SOL_SPKG="https://github.com/streamingfast/firehose-solana/releases/download/v1.1.3/solana-v0.1.4.spkg"
-substreams protogen "$SOL_SPKG" --exclude-paths="sf/substreams/rpc,sf/substreams/v1,google/" --output-path="./core/src/pb"
+substreams protogen "$SOL_SPKG" --exclude-paths="sf/substreams/rpc,sf/substreams/options.proto,sf/substreams/sink,sf/substreams/index,sf/substreams/v1,google/" --output-path="./core/src/pb"
